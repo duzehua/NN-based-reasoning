53eec48669b2cef35a19b233c6b283f26935191c
Jenhonnnnnn <33522266+Jenhonnnnnn@users.noreply.github.com>
2022-09-28 19:42:14 +0800

    运算符替换为函数表达

--- FullyConnectedFunction.h
+++ FullyConnectedFunction.h
@@ -1,8 +1,9 @@
 #ifndef FULLYCONNECTEDFUNCTION_H
 #define FULLYCONNECTEDFUNCTION_H
 
-    #include "Tensor.h"
-    void TensorFlatten(Tensor *conved_Tsr, Tensor *flatten_Data);
-    void FullConnected(Tensor *m_data_in, Tensor *weight, Tensor *bias, Tensor *Tensor);
+#include "Tensor.h"
+#include "FloatFunction.h"
+void TensorFlatten(Tensor *conved_Tsr, Tensor *flatten_Data);
+void FullConnected(Tensor *m_data_in, Tensor *weight, Tensor *bias, Tensor *Tensor);
 
 #endif
